acd2ad358646d045f2444b528f92d6362097dd19
nisha-jangir <jangir.nishu@gmail.com>
2024-05-23 11:50:20 +0530

    adding assignment 1

---
+++ remove_duplicate_array/index.js
@@ -0,0 +1,12 @@
+let arr = ["apple", "mango", "apple", "orange", "mango", "mango"];
+
+function removeDuplicates(arr) {
+  let unique = [];
+  for (i = 0; i < arr.length; i++) {
+    if (unique.indexOf(arr[i]) === -1) {
+      unique.push(arr[i]);
+    }
+  }
+  return unique;
+}
+console.log(removeDuplicates(arr));
